0599104bf69228ed4c0bad90300eed0ee963937c
Smilers <monmorti@gmail.com>
2020-03-31 20:51:31 +0300

    issue controller

---
+++ src/TimeTracker.WebHost/Controllers/IssueController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Application.Issues.Commands.CreateIssue;
+
+namespace TimeTracker.WebHost.Controllers
+{
+    public class IssueController : BaseController
+    {
+        public async Task<IActionResult> New(CreateIssueCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
