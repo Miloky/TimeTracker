7d5b586e64d5f1d41229645f5f98b1f2255d72c1
Smilers <monmorti@gmail.com>
2020-04-02 04:42:17 +0300

    issue endpoint

--- src/TimeTracker.WebHost/Controllers/IssueController.cs
+++ src/TimeTracker.WebHost/Controllers/IssueController.cs
@@ -1,15 +1,38 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using TimeTracker.Application.Issues.Commands.CreateIssue;
+using TimeTracker.Application.Issues.Queries.IssueList;
+using TimeTracker.Application.WorkLogs.Commands.StartWorkLog;
 
 namespace TimeTracker.WebHost.Controllers
 {
     public class IssueController : BaseController
     {
+        [HttpPost]
         public async Task<IActionResult> New(CreateIssueCommand command)
         {
             var result = await Mediator.Send(command);
             return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> List(IssueListQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StartLog(StartLogCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StopLog()
+        {
+            return Ok();
         }
     }
 }
