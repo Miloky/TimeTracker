8ef469d448cdc106254165e930671ec3077e0741
Smilers <monmorti@gmail.com>
2020-04-02 04:40:03 +0300

    WebHost Tests Seed

---
+++ src/TimeTracker.WebHost.FunctionalTest/Common/CustomWebApplicationFactory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TimeTracker.Application.Interfaces;
+using TimeTracker.Persistence;
+
+namespace TimeTracker.WebHost.FunctionalTest.Common
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Create a new service provider.
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                services.AddDbContext<ITimeTrackerDbContext, TimeTrackerDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+                //Build the service provider
+                var sp = services.BuildServiceProvider();
+
+                //Create a scope to obtain a reference  to the database
+                // context (TimeTrackerDbContext)
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var context = scopedServices.GetRequiredService<ITimeTrackerDbContext>();
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    var concreteContext = (TimeTrackerDbContext)context;
+                    concreteContext.Database.EnsureCreated();
+
+                    //try
+                    //{
+                        Utilities.InitializeDbForTest(concreteContext);
+                        // Seed database
+                    //}
+                    //catch (Exception ex)
+                    //{
+                        // log error
+                    //}
+                }
+
+            });
+        }
+    }
+}
---
+++ src/TimeTracker.WebHost.FunctionalTest/Controllers/Project/Create.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using TimeTracker.Application.Projects.Commands.CreateProject;
+using TimeTracker.WebHost.FunctionalTest.Common;
+using Xunit;
+
+namespace TimeTracker.WebHost.FunctionalTest.Controllers.Project
+{
+    public class Create : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _httClient;
+
+        public Create(CustomWebApplicationFactory<Startup> factory)
+        {
+            _httClient = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GivenCreateProjectCommand_ReturnsNewProjectId()
+        {
+            var command = new CreateProjectCommand
+            {
+                Name = "TEST DATA"
+            };
+
+            var content = Utilities.GetRequestContent(command);
+            var response = await _httClient.PostAsync("api/projects/new", content);
+            response.EnsureSuccessStatusCode();
+            var responseObject = await Utilities.GetResponseContent<JObject>(response);
+            var projectId = responseObject.Value<int>("id");
+            Assert.Single(responseObject);
+            Assert.NotEqual(0, projectId);
+            Assert.Equal(3, projectId);
+        }
+    }
+}
