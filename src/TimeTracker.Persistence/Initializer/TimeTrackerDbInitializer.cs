5364cce61a492cb663142023d42f716e2714c574
Smilers <monmorti@gmail.com>
2020-04-05 20:58:24 +0300

    initilizer from sql to POCO classes

--- src/TimeTracker.Persistence/Initializer/TimeTrackerDbInitializer.cs
+++ src/TimeTracker.Persistence/Initializer/TimeTrackerDbInitializer.cs
@@ -1,32 +1,82 @@
-using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using Microsoft.EntityFrameworkCore;
-using TimeTracker.Application.Interfaces;
 using TimeTracker.Domain.Entities;
 
 namespace TimeTracker.Persistence.Initializer
 {
     public class TimeTrackerDbInitializer
     {
+        private readonly IDictionary<int, Project> _projects = new Dictionary<int, Project>();
+
         public static void Seed(TimeTrackerDbContext context)
         {
-            SeedProjects(context);
+            if (context.Projects.FirstOrDefault() != null) return;
+            var initilizer = new TimeTrackerDbInitializer();
+            initilizer.SeedEverything(context);
         }
 
-        private static void SeedProjects(TimeTrackerDbContext context)
-        {    
-            if (context.Projects.FirstOrDefault()!= null) return;
-            string sql = ReadSqlFile("projects.sql");
-            context.Database.ExecuteSqlCommand(sql);
+        private void SeedEverything(TimeTrackerDbContext context)
+        {
+            SeedProjects(context);
+            SeedIssues(context);
+        }
+
+        private void SeedIssues(TimeTrackerDbContext context)
+        {
+            var issues = new Issue[]
+            {
+                new Issue
+                {
+                    Title = "Harry Potter and philosopher's stone"
+                },
+                new Issue
+                {
+                    Title = "Harry Potter and philosopher's stone",
+                }
+            };
+
+            context.Issues.AddRange(issues);
+
+            foreach (Issue issue in issues)
+            {
+                issue.Project = _projects[1];
+                issue.Identifier = $"{issue.Project.Prefix}-{issue.Id}";
+            }
+
+            context.SaveChanges();
+        }
+
+        private void SeedProjects(TimeTrackerDbContext context)
+        {
+            _projects.Add(1, new Project
+            {
+                Prefix = "ENG",
+                Title = "English  study"
+            });
+            _projects.Add(2, new Project
+            {
+                Prefix = "JS",
+                Title = "Javascript learning path"
+            });
+            _projects.Add(3, new Project
+            {
+                Prefix = "EB",
+                Title = "English books"
+            });
+
+            foreach (Project project in _projects.Values)
+            {
+                context.Projects.Add(project);
+            }
             context.SaveChanges();
         }
 
         private static string ReadSqlFile(string name)
         {
             var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-             return File.ReadAllText(Path.Combine(assemblyPath, "Initializer", name));
+            return File.ReadAllText(Path.Combine(assemblyPath, "Initializer", name));
         }
     }
 }
--- src/TimeTracker.Persistence/Initializer/projects.sql
+++
@@ -1,3 +0,0 @@
-insert into projects(Prefix, Title) values("ENG", "English study");
-insert into projects(Prefix, Title) values("JS", "JavaScript learning path");
-insert into projects(Prefix, Title) values("BK", "English books");
