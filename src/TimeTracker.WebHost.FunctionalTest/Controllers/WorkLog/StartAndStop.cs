dda3dd93fba346bcb9ba68ec522ce03b6aaead91
Smilers <monmorti@gmail.com>
2020-04-12 22:29:17 +0300

    rename file

--- src/TimeTracker.WebHost.FunctionalTest/Controllers/WorkLog/StartAndStop.cs
+++ src/TimeTracker.WebHost.FunctionalTest/Controllers/WorkLog/StartAndStop.cs
@@ -2,12 +2,13 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using TimeTracker.Application.WorkLogs.Commands.StartWorkLog;
+using TimeTracker.Application.WorkLogs.Commands.StopWorkLog;
 using TimeTracker.WebHost.FunctionalTest.Common;
 using Xunit;
 
 namespace TimeTracker.WebHost.FunctionalTests.Controllers.Worklog
 {
-    public class Create:IClassFixture<CustomWebApplicationFactory<Startup>>
+    public class Create : IClassFixture<CustomWebApplicationFactory<Startup>>
     {
         private readonly HttpClient _client;
 
@@ -35,7 +36,33 @@ namespace TimeTracker.WebHost.FunctionalTests.Controllers.Worklog
             //Assert
             response.EnsureSuccessStatusCode();
             Assert.Equal("ENG-1", result.Identifier);
-            Assert.Equal(now,result.Start);
+            Assert.Equal(now, result.Start);
+        }
+
+        [Fact]
+        public async Task GivenStopLogCommand_ReturnStopLogResult()
+        {
+            // Arrange
+            var start = DateTime.Parse("04/05/2020 20:00:00");
+            var end = DateTime.Parse("04/05/2020 23:00:00");
+            var duration = (end - start).Minutes;
+            var command = new StopLogCommand
+            {
+                Identifier = "JS-2",
+                End = end
+            };
+
+
+            //Act
+            var requestContent = Utilities.GetRequestContent(command);
+            var response = await _client.PostAsync("api/issue/stoplog", requestContent);
+            var result = await Utilities.GetResponseContent<StopLogCommandResult>(response);
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(duration,result.Duration);
+            Assert.Equal(start,result.Start);
+            Assert.Equal(end,result.End);
         }
     }
 }
