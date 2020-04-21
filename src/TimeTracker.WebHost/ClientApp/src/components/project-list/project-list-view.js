57329754131ad28581fb124a8ee2f15b38fdfcfa
Smilers <monmorti@gmail.com>
2020-04-21 22:22:24 +0300

    eslint formatting

--- src/TimeTracker.WebHost/ClientApp/src/components/project-list/project-list-view.js
+++ src/TimeTracker.WebHost/ClientApp/src/components/project-list/project-list-view.js
@@ -5,39 +5,36 @@ import { Link } from 'react-router-dom';
 import Project from '../project';
 import classes from './project-list.module.scss';
 
-
 export default class ProjectList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: false,
-            projects: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      projects: []
+    };
+  }
+  async componentDidMount() {
+    this.setState({ loading: true });
+    try {
+      const { data } = await ProjectService.fetchProjectsAsync();
+      this.setState({ projects: data });
+    } catch (error) {
+      console.log('Error while fetching projects: ', error);
+    } finally {
+      this.setState({ loading: false });
     }
-    async componentDidMount() {
-        this.setState({ loading: true })
-        try {
-            const { data } = await ProjectService.fetchProjectsAsync();
-            this.setState({ projects: data });
-        } catch (error) {
-            console.log('Error while fetching projects: ', error);
-        }
-        finally {
-            this.setState({ loading: false });
-        }
-    }
+  }
 
-    render() {
-        return <Fragment>
-            {
-                this.state.loading ? <div>Loading...</div> : null
-            }
-            <div className={classes.project_list}>
-                {
-                    this.state.projects.length && !this.state.loading ? this.state.projects
-                        .map(project => <Project key={project.id} project={project} />) : null
-                }
-            </div>
-        </Fragment>
-    }
+  render() {
+    return (
+      <Fragment>
+        {this.state.loading ? <div>Loading...</div> : null}
+        <div className={classes.project_list}>
+          {this.state.projects.length && !this.state.loading
+            ? this.state.projects.map(project => <Project key={project.id} project={project} />)
+            : null}
+        </div>
+      </Fragment>
+    );
+  }
 }
