d084e43c51bbce03c7458bb7fa0f15eac0024d57
Smilers <monmorti@gmail.com>
2020-04-21 22:21:57 +0300

    count down

--- src/TimeTracker.WebHost/ClientApp/src/components/countdown/countdown-view.js
+++ src/TimeTracker.WebHost/ClientApp/src/components/countdown/countdown-view.js
@@ -1,55 +1,98 @@
-import React, { useState, Fragment, useMemo, useEffect } from 'react';
+import React, { useState, Fragment, useCallback, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
+import { useInterval } from '../../hooks';
 import classes from './countdown.module.scss';
 
-function getDateDifference(start) {
-  var now = new Date().getTime();
-  var timeSpan = now - start;
-
-  var days = Math.floor(timeSpan / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((timeSpan % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((timeSpan % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((timeSpan % (1000 * 60)) / 1000);
-  return {
-    days,
-    hours,
-    minutes,
-    seconds
+// TODO: UTC
+function getDateDifference(initialMin, start) {
+  const toDiffObj = val => {
+    const days = Math.floor(val / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((val % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((val % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((val % (1000 * 60)) / 1000);
+    return {
+      days,
+      hours,
+      minutes,
+      seconds
+    };
   };
+
+  const initialMilliseconds = initialMin * 60 * 1000;
+  const diffMiliseconds = start ? new Date() - start : 0;
+  const initial = toDiffObj(initialMilliseconds);
+  const diff = toDiffObj(diffMiliseconds);
+  return Worklog.sum(initial, diff);
+}
+getDateDifference.default = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+
+function Worklog(days, hours, minutes, seconds) {
+  if (arguments.length < 4) {
+    throw new Error('Invalid arguments exception');
+  }
+  this.days = days;
+  this.hours = hours;
+  this.minutes = minutes;
+  this.seconds = seconds;
 }
 
-export default function Countdown({ startDate }) {
-  const [span, setSpan] = useState(getDateDifference(startDate));
+Worklog.getDefault = function () {
+  return new Worklog(0, 0, 0, 0);
+};
+
+Worklog.sum = function (w1, w2) {
+  const totalSeconds = w1.seconds + w2.seconds;
+  const seconds = totalSeconds % 60;
+  const secondsOverflow = totalSeconds - seconds;
+
+  const totalMinutes = w1.minutes + w2.minutes + Math.floor(secondsOverflow / 60);
+  const minutes = totalMinutes % 60;
+  const minutesOverflow = totalMinutes - minutes;
+
+  const totalHours = w1.hours + w2.hours + Math.floor(minutesOverflow / 60);
+  const hours = totalHours % 24;
+  const hoursOverflow = totalHours - hours;
+
+  const days = w1.days + w2.days + Math.floor(hoursOverflow / 24);
+
+  return new Worklog(days, hours, minutes, seconds);
+};
+
+export default function Countdown({ initial, active, start }) {
+  const [span, setSpan] = useState(getDateDifference(initial, start));
   // TODO: Change on requestAnimationFrame
   const updateCount = () => {
-    const diff = getDateDifference(startDate);
-    setSpan(diff);
+    if (active) {
+      const diff = getDateDifference(initial, start);
+      setSpan(diff);
+    }
   };
 
-  const updater = useMemo(() => setInterval(updateCount, 1000));
-
   useEffect(() => {
-    return () => {
-      clearInterval(updater);
-    };
-  });
+    setSpan(getDateDifference(initial, start));
+  }, [initial]);
+
+  useInterval(updateCount, 1000);
 
   return (
-    <div
-      className={classes.count}>
-        <span>{span.days}d </span>
-        <span>{span.hours}h </span>
-        <span>{span.minutes}m </span>
-        <span>{span.seconds}s</span>
-      </div>
+    <div className={classes.count}>
+      <span>{span.days}d </span>
+      <span>{span.hours}h </span>
+      <span>{span.minutes}m </span>
+      <span>{span.seconds}s</span>
+    </div>
   );
 }
 
 Countdown.propTypes = {
-  startDate: PropTypes.instanceOf(Date)
+  initial: PropTypes.number,
+  active: PropTypes.bool,
+  start: PropTypes.instanceOf(Date)
 };
 
 Countdown.defaultProps = {
-  startDate: new Date()
+  start: null,
+  initial: 0,
+  active: false
 };
