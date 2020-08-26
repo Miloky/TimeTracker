886fb1519e60a1d77081b4da1dde15ab109a3229
Miloky <monmorti@gmail.com>
2020-08-26 23:57:28 +0300

    Login mockup

--- src/TimeTracker.WebHost/Controllers/AccountController.cs
+++ src/TimeTracker.WebHost/Controllers/AccountController.cs
@@ -5,9 +5,43 @@ namespace TimeTracker.WebHost.Controllers
 {
     public class AccountController : BaseController
     {
-        public async Task<IActionResult> Login()
+        private readonly IUserManager<IdentityUser> _userManager;
+        public AccountController(IUserManager<IdentityUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] CredentialsViewModel credentials)
+        {
+            var user = await _userManager.FindByEmailAsync(credentials.Email);
+            if(user==null){
+                // TODO: Create api error model and return it
+                return BadRequest();
+            }
             return Ok();
         }
+    }
+
+    public class CredentialsViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class IdentityUser : IdentityUser<int>
+    {
+
+    }
+
+    public class IdentityUser<T>
+    {
+        public T Id { get; set; }
+    }
+
+    public interface IUserManager<TUser>
+    {
+        Task<TUser> FindByNameAsync(string name);
+        Task<TUser> FindByEmailAsync(string email);
     }
 }
