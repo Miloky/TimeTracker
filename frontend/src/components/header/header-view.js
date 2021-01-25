dccc80f5c5f688faf6ac82559c2db6392288b328
Miloky <monmorti@gmail.com>
2021-01-26 01:51:34 +0200

    to AppBar

--- frontend/src/components/header/header-view.js
+++ frontend/src/components/header/header-view.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.scss';
 import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, IconButton, Drawer } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  const close = () => {
+    setOpen(false);
+  };
+
+  const open = () => {
+    setOpen(true);
+  };
+
   return (
-    <header className='test'>
-      <Link to='/' style={{ color: 'white' }}>
-        HOme
-      </Link>
-      <Link to='/create-project' style={{ color: 'white' }}>
-        Create project
-      </Link>
-      <Link to='/create-issue' style={{ color: 'white' }}>
-        Create issue
-      </Link>
-    </header>
+    <>
+      <AppBar>
+        <Toolbar>
+          <IconButton onClick={open}>
+            <MenuIcon />
+          </IconButton>
+          <Link to='/' style={{ color: 'white' }}>
+            HOme
+          </Link>
+          <Link to='/create-project' style={{ color: 'white' }}>
+            Create project
+          </Link>
+          <Link to='/create-issue' style={{ color: 'white' }}>
+            Create issue
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={isOpen} onClose={close}>
+        Test
+      </Drawer>
+    </>
   );
 };
 
