8329a46cec0b1fbc024c988cd708e70d1b5c3620
Miloky <monmorti@gmail.com>
2021-01-26 00:55:02 +0200

    to functional component

--- frontend/src/components/header/header-view.js
+++ frontend/src/components/header/header-view.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './header.scss';
 import { Link } from 'react-router-dom';
 
-function Header() {
-  return <header className='test'>
-    <Link to='/' style={{ color: 'white' }}>HOme</Link>
-    <Link to='/create-project' style={{ color: 'white' }}>Create project</Link>
-    <Link to='/create-issue' style={{ color: 'white' }}>Create issue</Link>
-  </header>;
-}
+const Header = () => {
+  return (
+    <header className='test'>
+      <Link to='/' style={{ color: 'white' }}>
+        HOme
+      </Link>
+      <Link to='/create-project' style={{ color: 'white' }}>
+        Create project
+      </Link>
+      <Link to='/create-issue' style={{ color: 'white' }}>
+        Create issue
+      </Link>
+    </header>
+  );
+};
 
 export default Header;
